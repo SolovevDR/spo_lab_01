0e191ceae4ff4b544742eede7c931b0637c88cfd
Danila <rediska047@mail.ru>
2021-09-02 20:20:19 +0300

    lab_01

---
+++ lab_01.cpp
@@ -0,0 +1,87 @@
+﻿// lab_01.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdio.h>
+#define WINVER 0x0A00
+#include <windows.h>
+
+#define BUFSIZE MAX_PATH
+#define FILESYSNAMEBUFSIZE MAX_PATH
+#define MAX_KEY_LENGTH 255
+
+using namespace std;
+
+#define INFO_BUFFER_SIZE 32767
+
+int main()
+{
+    DWORD dwVersion = 0;
+    DWORD dwMajorVersion = 0;
+    DWORD dwMinorVersion = 0;
+    DWORD dwBuild = 0;
+
+    dwVersion = GetVersion();
+
+    // Get the Windows version.
+
+    dwMajorVersion = (DWORD)(LOBYTE(LOWORD(dwVersion)));
+    dwMinorVersion = (DWORD)(HIBYTE(LOWORD(dwVersion)));
+
+    // Get the build number.
+
+    if (dwVersion < 0x80000000)
+        dwBuild = (DWORD)(HIWORD(dwVersion));
+
+    printf("Version is %d.%d (%d)\n",
+        dwMajorVersion,
+        dwMinorVersion,
+        dwBuild);
+
+   
+    TCHAR szPath[_MAX_PATH] = { 0 };
+    UINT iRet = GetSystemDirectory(szPath, _MAX_PATH);
+    printf("Result function GetSystemDirectory %d\n", iRet);
+    
+    char buffer[256];
+    DWORD size = 256;
+    GetComputerNameA(buffer, &size);
+    printf("%s\n", buffer);
+
+    GetUserNameA(buffer, &size);
+    printf("%s\n", buffer);
+
+    char buffer2[MAX_PATH + 1];
+    DWORD size2 = MAX_PATH;
+    char timeBuffer[MAX_PATH + 1];
+    DWORD  CBufLen = MAX_PATH;
+    __int64 total, available, free;
+
+    HANDLE firstVolume =  FindFirstVolumeA(buffer2, size2);
+
+    do {
+        printf("\n      %s", buffer2);
+        GetVolumePathNamesForVolumeNameA(buffer2, timeBuffer, CBufLen, &CBufLen);
+        char* path = timeBuffer;
+        printf("\npath: %s", path);
+        GetDiskFreeSpaceExA(buffer, (PULARGE_INTEGER)&available, (PULARGE_INTEGER)&total, (PULARGE_INTEGER)&free);
+        printf("\nsize: %u  bytes ", total);
+        printf("\nFree space: %u  bytes\n", available);
+
+
+
+    } while (FindNextVolumeA(firstVolume, buffer2, size2));
+    FindVolumeClose(firstVolume);
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
